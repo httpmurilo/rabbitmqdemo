af58605542a36f40ad787c83d5df7d0c19a17672
Murilo Eduardo <muriloeduardo1997@outlook.com>
2020-04-20 21:04:47 -0300

    first commit

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -5,8 +5,7 @@ using RabbitMQ.Client.Events;
 
 namespace Consumer {
     class Program {
-        static void Main (string[] args) 
-        {
+        static void Main (string[] args) {
             var factory = new ConnectionFactory () { HostName = "localhost" };
             using (var connection = factory.CreateConnection ())
             using (var channel = connection.CreateModel ()) {
@@ -18,17 +17,27 @@ namespace Consumer {
 
                 var consumer = new EventingBasicConsumer (channel);
                 consumer.Received += (model, ea) => {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString (body);
-                    Console.WriteLine (" [x] Received {0}", message);
+
+                    {
+                        try
+
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString (body);
+                        Console.WriteLine (" [x] Received {0}", message);
+                    } catch (Exception ex) {
+                        //em caso de ex retorno pra fila
+
+                        channel.BasicAck (ea.DeliveryTag, false);
+                    }
                 };
                 channel.BasicConsume (queue: "qwebapp",
-                    autoAck : true,
+                    autoAck : false,
                     consumer : consumer);
 
                 Console.WriteLine (" Press [enter] to exit.");
                 Console.ReadLine ();
-            }
+            };
         }
     }
+}
 }
